15d7fc242e9de593ce1a8540f26046e16c24e2f4
GraySpace <alexandergray37@gmail.com>
2020-11-27 20:07:53 -0500

    users can join and leave chats

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
-//app.js
 const io = require('socket.io')(server);
+//We'll store our online users here
+let onlineUsers = {};
 io.on("connection", (socket) => {
-  // This file will be read on new socket connections
-    require('./sockets/chat.js')(io, socket);
+  // Make sure to send the users to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers);
 })
 
 //app.js
